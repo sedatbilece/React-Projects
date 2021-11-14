24551a6d46a8ae9c9744f9e8f363f47568b1dd1b
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-11-14 11:21:40 +0300

    Props Proje Başlangıç

---
+++ props/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const  App=() =>{
+
+    return (
+
+        <div>
+
+            hello world
+        </div>
+    )
+}
+
+
+ReactDOM.render(
+
+    <App /> ,
+    document.querySelector("#root")
+)
