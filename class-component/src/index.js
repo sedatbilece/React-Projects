9390d2818c115e671719b59c895b7dbf2c30f54e
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-11-18 17:10:42 +0300

    ..

---
+++ class-component/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const App=()=>{
+
+
+    return(<div>
+        
+       <div className="ui botton">deneme</div>
+    </div>)
+}
+
+
+ReactDOM.render(
+
+    <App/>,
+    document.querySelector("#root")
+)
