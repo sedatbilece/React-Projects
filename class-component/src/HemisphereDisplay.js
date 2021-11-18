0f9d2c9820b0b7babd5ac90752304e7ee99e381f
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-11-18 17:10:26 +0300

    .

---
+++ class-component/src/HemisphereDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const HemisphereDisplay=()=>{
+
+
+    return(<div>
+
+kuzey yarımküre
+    </div>);
+}
+
+export default HemisphereDisplay;
