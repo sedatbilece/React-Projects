75381a8b227ac6cde112ff7c1921ca4f0ef15dcb
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-12-07 10:53:32 +0300

    react image ve file kullanımı

--- class-component/src/HemisphereDisplay.js
+++ class-component/src/HemisphereDisplay.js
@@ -1,17 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-
+import southernpic from "./images/southern.jpg";
+import northernpic from "./images/northen.jpg";
 const HemisphereDisplay=({latidute})=>{ // {latidute } props.latidute alınır sadece props ile gelen verilerden
 
 
 const hemisphere= latidute >0 ? "kuzey yarımküre ": "güney yarımküre" ;
               // latidute büyük sıfır ise  ? ----koşul ---- : --değil ise koşulu -----
+
+
+    const picture =latidute>0? northernpic : southernpic;
     return(<div>
 
     {hemisphere+"  : "}
 
 <span>{latidute}</span>
+
+<div>
+   <img src={picture}></img>
+</div>
     </div>);
 }
 
