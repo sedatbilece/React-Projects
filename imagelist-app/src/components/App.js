d7190ac24299c2ac31c35eeab757b18d199e17a3
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-12-19 15:24:36 +0300

    search comp ve css ayarlandı

--- imagelist-app/src/components/App.js
+++ imagelist-app/src/components/App.js
@@ -4,11 +4,7 @@ const App=()=>{
 
 
     return(<div>
-<h1 className="bg-gray-200 rounded-full text-red-500">deneme</h1>
 
-<div className="bg-blue-200 rounded-full border-collapse text-center p-6 mt-10 inline-block">
-    text
-</div>
 <SearchInput />
     </div>
     )
--- imagelist-app/src/components/SearchInput.js
+++ imagelist-app/src/components/SearchInput.js
@@ -5,7 +5,20 @@ class SearchInput extends React.Component{
       render(){
 
             return ( <div>
-SearchInput componenti
+                
+                <form className="inline-block w-full mt-20 ml-64">
+
+                <div class="grid grid-cols-4 gap-4">
+                    <div>
+                    <input type="text" placeholder="Write Here !" className=" inline-block focus:outline-none focus:outline-cyan-300 focus:placeholder-cyan-500 shadow appearance-none border rounded w-96 py-2 px-3 text-gray-700 leading-tight " />
+                    </div>
+                    <div>
+                    <button type="submit" className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Search</button>
+                    </div>
+                     </div>
+                  
+                  
+                </form>
             </div>
             )
 
