642af11973d0f4a73bad2e38af5ed1821dfbb2dd
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2021-12-19 15:02:53 +0300

    componentler oluşturuldu gereksiz dosyalar silindi

---
+++ imagelist-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchInput from "./SearchInput";
+const App=()=>{
+
+
+    return(<div>
+<h1 className="bg-gray-200 rounded-full text-red-500">deneme</h1>
+
+<div className="bg-blue-200 rounded-full border-collapse text-center p-6 mt-10 inline-block">
+    text
+</div>
+<SearchInput />
+    </div>
+    )
+}
+
+export default App;
---
+++ imagelist-app/src/components/SearchInput.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class SearchInput extends React.Component{
+
+      render(){
+
+            return ( <div>
+SearchInput componenti
+            </div>
+            )
+
+      }
+     
+
+
+}
+
+
+export default SearchInput;
