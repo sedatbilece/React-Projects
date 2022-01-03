0444837ec0453a21470e6425f778bc600241fc06
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-01-03 16:23:30 +0300

    api resim çekme ve state güncelleme

--- imagelist-app/src/components/App.js
+++ imagelist-app/src/components/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import SearchInput from "./SearchInput";
 import axios from 'axios';
-
+import ImageList from "./ImageList";
 class App extends React.Component{
 
-    onSearchSubmit=(entry)=>{
-console.log(entry);
+    //state oluşturduk
+    state={ images : []  }
 
+    onSearchSubmit= async (entry)=>{
+
+     const res=  await  axios.get(`https://pixabay.com/api/?key=25000034-e6e5471e6e21c9f7f0e5d100a&q=${entry}&image_type=photo`);
+
+   
+// state güncellenince comp yeniden render edilir
+     this.setState ({ images:res.data.hits })
     }
 
 
@@ -16,7 +23,8 @@ console.log(entry);
         return(<div>
 
             <SearchInput  onSearchSubmit={this.onSearchSubmit}/> 
-            
+           
+            <ImageList images={this.state.images} />
                 </div>
                 )
 
