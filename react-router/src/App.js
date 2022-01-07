6670b96f26517a293d60b2198c4564786b5e1526
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-01-07 22:14:43 +0300

    react route kullanımı

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
+import {BrowserRouter as Router  ,Route} from 'react-router-dom';
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div >
+    <Router> 
+    
+    <div className="App" >
      <Navbar />
         <h1 className="text-3xl font-bold underline bg-cyan-200 rounded-lg p-7 text-black mt-10 inline-block ">
       Hello world!
     </h1>
-
+        <Route path='/'  component={Home} />
      
-    </div>
+    </div></Router>
   );
 }
 
