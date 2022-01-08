1b74ee95b78207f7da132ae6c303add0dc40018a
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-01-08 10:37:20 +0300

    sayfa yenilemeyi önlemek

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link,NavLink} from 'react-router-dom';
 
 
 
@@ -12,12 +13,13 @@ return(<div>
     ['About', '/About'],
     ['Contact', '/Contact'],
   ].map(([title, url]) => (
-    <a href={url} className="rounded-lg px-3 py-2 text-gray-700 font-medium hover:bg-gray-100 hover:text-gray-900">{title}</a>
+    <NavLink to={url} className="rounded-lg px-3 py-2 text-gray-700 font-medium hover:bg-gray-100 hover:text-gray-900">{title}</NavLink>
   ))}
 </nav>
 
 </div>)
 
 }
+// Link NavLink farkı : render esnasında NavLinkte <a> tagine class="active" eklenir css için kullanılabilir
 
 export default Navbar;
